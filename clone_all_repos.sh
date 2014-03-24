7d6631620cb06431c1034f8a598a8d5428d0f05d
WooParadog <guohaochuan@gmail.com>
2014-03-24 21:19:11 +0800

    add option to update

--- clone_all_repos.sh
+++ clone_all_repos.sh
@@ -2,13 +2,19 @@
 #
 # clone_all_repos.sh
 
+flag=$1
+
 for i in `cat repos`;do
     target_dir=`echo $i | sed -e 's/^.*\/\(.*\).git/\1/g'`
     if [ -d $target_dir ];then
-        cd $target_dir
-        git pull origin master
-        cd ..
+        if [ "update" = "$flag" ]; then
+            echo Updating, $i
+            cd $target_dir
+            git pull origin master
+            cd ..
+        fi
     else
+        echo git submodule add $i $target_dir
         git submodule add $i $target_dir
     fi
 done
